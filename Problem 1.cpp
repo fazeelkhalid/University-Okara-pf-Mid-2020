2837b732d13601fb0bcb54ec806adb091a775182
Fazeel Khalid <fazeelkhalid9@gmail.com>
2021-02-08 22:37:17 +0500

    Update Problem 1.cpp

--- Problem 1.cpp
+++ Problem 1.cpp
@@ -1,18 +1,1 @@
-#include<iostream>
-
-using namespace std;
-
-int main() {
-	int number = 0;
-	cout << "Enter a number : ";
-	cin >> number;
-	if (number % 5 == 0 && number != 0) {
-		cout << "\nHiFive";
-	}
-	else if (number % 2 == 0 && number != 0) {
-		cout << "\nHiEven";
-	}
-	else {
-		cout << "\nNot a multiple of 5 and 2 ";
-	}
-}
+qw
